45a31c75768e49b49969245a6d6fd7c8340226b2
ike-blessing <blessingfavour308@gmail.comclear>
2023-10-03 01:41:11 +0100

    created nav.jsx

--- src/assets/nav.jsx
+++ src/assets/nav.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/images/logo.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function nav() {
   const [count, setCount] = useState(0)
 
   return (
@@ -19,22 +18,9 @@ function App() {
             <button className="p-1 m-2 bg-black text-base text-white rounded">Creat an account</button>
           </div>
       </div>
-    </div>       
-
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
 
-export default App
+export default nav
